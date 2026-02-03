ab23597e6f879a11de21b1920b01b89924566177
saipuji26 <saipuji2006@gmail.com>
2026-02-03 21:20:55 +0530

    water_jug-2.py

--- WEEK2/water_jug-2.py
+++ WEEK2/water_jug-2.py
@@ -1,5 +1,4 @@
 import heapq
-
 def water_jug_astar(A, B, target):
     h = lambda x, y: abs(target - x)
     moves = lambda x, y: [
@@ -7,10 +6,8 @@ def water_jug_astar(A, B, target):
         (x - min(x, B - y), y + min(x, B - y)),
         (x + min(y, A - x), y - min(y, A - x))
     ]
-
     pq = [(0, 0, 0, [(0, 0)])]
     visited = set()
-
     while pq:
         f, x, y, path = heapq.heappop(pq)
         if x == target:
@@ -18,15 +15,12 @@ def water_jug_astar(A, B, target):
         if (x, y) in visited:
             continue
         visited.add((x, y))
-
         for nx, ny in moves(x, y):
             if (nx, ny) not in visited:
                 g = len(path)
                 heapq.heappush(pq, (g + h(nx, ny), nx, ny, path + [(nx, ny)]))
-
     return None
-
-
 # Space station problem
 result = water_jug_astar(7, 4, 6)
 print(result)
+
