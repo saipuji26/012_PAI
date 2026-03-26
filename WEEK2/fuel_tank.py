5537a931fc6c1c27d809505db288d9b4fcc7efd9
Mangu Sai Pujitha <saipuji2006@gmail.com>
2026-03-26 06:06:47 +0530

    fuel_tank.py

--- WEEK2/fuel_tank.py
+++ WEEK2/fuel_tank.py
@@ -1,27 +1,28 @@
 import heapq
-def fuel_blend_astar(tankA, tankB, target_octane):
-    heuristic = lambda a, b: abs(target_octane - a)
-    operations = lambda a, b: [
-        (tankA, b), (a, tankB),     
-        (0, b), (a, 0),             
-        (a - min(a, tankB - b), b + min(a, tankB - b)),  
-        (a + min(b, tankA - a), b - min(b, tankA - a))   
-    ]
-    pq = [(0, 0, 0, [(0, 0)])]   
-    visited = set()
-    while pq:
-        f, a, b, path = heapq.heappop(pq)
-        if a == target_octane:
-            return path
-        if (a, b) in visited:
-            continue
-        visited.add((a, b))
-        for na, nb in operations(a, b):
-            if (na, nb) not in visited:
-                g = len(path)
-                h = heuristic(na, nb)
-                heapq.heappush(pq, (g + h, na, nb, path + [(na, nb)]))
-    return None
-result = fuel_blend_astar(7, 4, 6)
-print(result)
-
+A = int(input("Tank A: "))
+B = int(input("Tank B: "))
+T = int(input("Target: "))
+pq = [(0, 0, 0, [(0, 0)])]
+vis = set()
+while pq:
+    f, a, b, path = heapq.heappop(pq)
+    if a == T:
+        print("Solution:", path)
+        break
+    if (a, b) in vis:
+        continue
+    vis.add((a, b))
+    for na, nb in [
+        (A, b), (a, B), (0, b), (a, 0),
+        (a - min(a, B-b), b + min(a, B-b)),
+        (a + min(b, A-a), b - min(b, A-a))
+    ]:
+        if (na, nb) not in vis:
+            heapq.heappush(pq, (len(path)+abs(T-na), na, nb, path+[(na, nb)]))
+else:
+    print("No solution")")
+Output:-
+Tank A: 4
+Tank B: 3
+Target: 2
+Solution: [(0, 0), (0, 3), (3, 0), (3, 3), (4, 2), (0, 2), (2, 0)]
